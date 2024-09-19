152b397348e4b8ad7435571f23e67bd34a3251ee
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-19 07:13:02 -0700

    Create data types.cpp

---
+++ Data Types/data types.cpp
@@ -0,0 +1,23 @@
+#include <iostream.h>
+
+int main() {
+    int integerVar = 42;              // Integer
+    float floatVar = 3.14f;           // Float
+    double doubleVar = 3.14159265;    // Double
+    char charVar = 'C';               // Character
+    bool boolVar = true;              // Boolean
+    short shortVar = 32000;           // Short Integer
+    long longVar = 1234567890;        // Long Integer
+    unsigned int unsignedIntVar = 100; // Unsigned Integer
+
+    cout << "Integer: " << integerVar << endl;
+    cout << "Float: " << floatVar << endl;
+    cout << "Double: " << doubleVar << endl;
+    cout << "Char: " << charVar << endl;
+    cout << "Boolean: " << boolVar << endl;
+    cout << "Short: " << shortVar << endl;
+    cout << "Long: " << longVar << endl;
+    cout << "Unsigned Int: " << unsignedIntVar << endl;
+
+    return 0;
+}
