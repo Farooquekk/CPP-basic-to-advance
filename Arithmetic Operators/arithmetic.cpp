d52365dd33a19b9b9e3572ac6b5dcb8038206dae
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-19 07:20:22 -0700

    Create arithmetic.cpp

---
+++ Arithmetic Operators/arithmetic.cpp
@@ -0,0 +1,19 @@
+#include <iostream.h>
+
+int main() {
+    int a = 10, b = 5;
+
+    int sum = a + b;        // Addition
+    int diff = a - b;       // Subtraction
+    int product = a * b;    // Multiplication
+    int quotient = a / b;   // Division
+    int remainder = a % b;  // Modulus (remainder)
+
+    cout << "Sum: " << sum << endl;
+    cout << "Difference: " << diff << endl;
+    cout << "Product: " << product << endl;
+    cout << "Quotient: " << quotient << endl;
+    cout << "Remainder: " << remainder << endl;
+
+    return 0;
+}
