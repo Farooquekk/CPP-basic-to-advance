9158d87b58af1bd2cebad350ebded31404eae1ee
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-19 07:11:11 -0700

    Create variables.cpp

---
+++ Variables/variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream.h>
+
+int main() {
+    int integerVar = 10;
+    float floatVar = 3.14f;
+    double doubleVar = 3.14159;
+    char charVar = 'A';
+    bool boolVar = true;
+
+    cout << "Integer: " << integerVar << endl;
+    cout << "Float: " << floatVar << endl;
+    cout << "Double: " << doubleVar << endl;
+    cout << "Char: " << charVar << endl;
+    cout << "Boolean: " << boolVar << endl;
+
+    return 0;
+}
