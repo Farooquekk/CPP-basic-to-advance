2bfabdfc5ce5640cd2c02b0d6eb2f89ed692c911
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-20 05:10:29 -0700

    Create logical operators.cpp

---
+++ Logical Operators/logical operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream.h>
+
+int main() {
+    bool x = true, y = false;
+
+    cout << "x && y: " << (x && y) << endl;  // Logical AND
+    cout << "x || y: " << (x || y) << endl;  // Logical OR
+    cout << "!x: " << (!x) << endl;          // Logical NOT
+
+    return 0;
+}
