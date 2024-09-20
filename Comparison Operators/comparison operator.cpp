56fa83b446adfd197313f093d49da38be028cd0c
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-20 05:08:58 -0700

    Create comparison operator.cpp

---
+++ Comparison Operators/comparison operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream.h>
+
+int main() {
+    int a = 10, b = 5;
+
+    cout << "a == b: " << (a == b) << endl;  // Equal to
+    cout << "a != b: " << (a != b) << endl;  // Not equal to
+    cout << "a > b: " << (a > b) << endl;    // Greater than
+    cout << "a < b: " << (a < b) << endl;    // Less than
+    cout << "a >= b: " << (a >= b) << endl;  // Greater than or equal to
+    cout << "a <= b: " << (a <= b) << endl;  // Less than or equal to
+
+    return 0;
+}
