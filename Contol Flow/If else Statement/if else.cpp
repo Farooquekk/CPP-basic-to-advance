19b464d77b45fa05861ecd6fb2589939f8cd5549
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-21 05:13:38 -0700

    Create if else.cpp

---
+++ Contol Flow/If else Statement/if else.cpp
@@ -0,0 +1,22 @@
+#include <iostream.h>
+
+int main() {
+    int num;
+
+    // Taking user input
+    cout << "Enter a number: ";
+    cin >> num;
+
+    // if-else if-else control flow
+    if (num > 0) {
+        cout << "The number is positive." << endl;
+    }
+    else if (num < 0) {
+        cout << "The number is negative." << endl;
+    }
+    else {
+        cout << "The number is zero." << endl;
+    }
+
+    return 0;
+}
