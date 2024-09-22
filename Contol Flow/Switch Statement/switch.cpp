f20e8a9e97f0d98454033944470c83a1026cadc9
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-22 06:12:19 -0700

    Update switch.cpp

--- Contol Flow/Switch Statement/switch.cpp
+++ Contol Flow/Switch Statement/switch.cpp
@@ -1,1 +1,40 @@
+#include <iostream.h>
+
+int main() {
+    int day;
+
+    // Taking user input
+    cout << "Enter a number (1-7) for the day of the week: ";
+    cin >> day;
+
+    // switch statement to determine the day
+    switch (day) {
+        case 1:
+            cout << "Monday" << endl;
+            break;
+        case 2:
+            cout << "Tuesday" << endl;
+            break;
+        case 3:
+            cout << "Wednesday" << endl;
+            break;
+        case 4:
+            cout << "Thursday" << endl;
+            break;
+        case 5:
+            cout << "Friday" << endl;
+            break;
+        case 6:
+            cout << "Saturday" << endl;
+            break;
+        case 7:
+            cout << "Sunday" << endl;
+            break;
+        default:
+            cout << "Invalid input! Please enter a number between 1 and 7." << endl;
+            break;
+    }
+
+    return 0;
+}
 
