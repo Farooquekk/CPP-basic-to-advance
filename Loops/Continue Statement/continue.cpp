bac98ed19adb842ba878e15b163369234542ed01
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-23 05:18:49 -0700

    Create continue.cpp

---
+++ Loops/Continue Statement/continue.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        if (i == 3) {
+            continue;  // Skip iteration when i == 3
+        }
+        cout << "Iteration " << i << endl;
+    }
+    return 0;
+}
