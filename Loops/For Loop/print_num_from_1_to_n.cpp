d9e3661af3f5de5591dac4200ec76ceefe0a280b
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-23 05:21:55 -0700

    Create print_num_from_1_to_n.cpp

---
+++ Loops/For Loop/print_num_from_1_to_n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cout << "Enter a number: ";
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        cout << i << " ";
+    }
+    return 0;
+}
