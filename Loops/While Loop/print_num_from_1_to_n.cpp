bad18bb9d6f67e48d06bdd9f4ddff37d24e1cb49
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-25 07:32:16 -0700

    Create print_num_from_1_to_n.cpp

---
+++ Loops/While Loop/print_num_from_1_to_n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, i = 1;
+    cout << "Enter a number: ";
+    cin >> N;
+
+    while (i <= N) {
+        cout << i << " ";
+        i++;
+    }
+    return 0;
+}
