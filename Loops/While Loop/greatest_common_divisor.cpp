04eb4ecf3d11253bc6954f7e3f9b711547cfd726
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-26 06:02:46 -0700

    Create greatest_common_divisor.cpp

---
+++ Loops/While Loop/greatest_common_divisor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    while (a != b) {
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
+    }
+
+    cout << "GCD is: " << a << endl;
+    return 0;
+}
