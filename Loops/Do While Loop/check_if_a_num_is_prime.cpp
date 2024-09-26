c203c19ea370ef2e965bb13bfad4980225855033
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-26 06:10:13 -0700

    Create check_if_a_num_is_prime.cpp

---
+++ Loops/Do While Loop/check_if_a_num_is_prime.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, i = 2, flag = 0;
+    cout << "Enter a number: ";
+    cin >> N;
+
+    if (N <= 1) {
+        cout << "Not a prime number" << endl;
+        return 0;
+    }
+
+    do {
+        if (N % i == 0) {
+            flag = 1;
+            break;
+        }
+        i++;
+    } while (i <= N / 2);
+
+    if (flag == 0)
+        cout << N << " is a prime number" << endl;
+    else
+        cout << N << " is not a prime number" << endl;
+
+    return 0;
+}
