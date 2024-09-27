342772dc5ce029f2f794f30a70fccf8f7149f61a
Farooquekk <22sw040@students.muet.edu.pk>
2024-09-27 06:19:23 -0700

    Create menu_driven_program.cpp

---
+++ Loops/Do While Loop/menu_driven_program.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice;
+    float num1, num2, result;
+
+    do {
+        cout << "\nMenu:\n";
+        cout << "1. Add\n";
+        cout << "2. Subtract\n";
+        cout << "3. Multiply\n";
+        cout << "4. Divide\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice >= 1 && choice <= 4) {
+            cout << "Enter two numbers: ";
+            cin >> num1 >> num2;
+        }
+
+        switch (choice) {
+            case 1:
+                result = num1 + num2;
+                cout << "Result: " << result << endl;
+                break;
+            case 2:
+                result = num1 - num2;
+                cout << "Result: " << result << endl;
+                break;
+            case 3:
+                result = num1 * num2;
+                cout << "Result: " << result << endl;
+                break;
+            case 4:
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    cout << "Result: " << result << endl;
+                } else {
+                    cout << "Error: Division by zero!" << endl;
+                }
+                break;
+            case 5:
+                cout << "Exiting..." << endl;
+                break;
+            default:
+                cout << "Invalid choice!" << endl;
+        }
+    } while (
