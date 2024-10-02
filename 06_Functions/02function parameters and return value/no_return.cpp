5cd577c693870426bc7816be1e6a142cc83ece9f
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-02 05:08:29 -0700

    Create no_return.cpp

---
+++ 06_Functions/02function parameters and return value/no_return.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void printMessage() {
+    cout << "This function doesn't return any value." << endl;
+}
+
+int main() {
+    printMessage();  // Call the void function
+    return 0;
+}
