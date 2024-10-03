25977d156c3c552cfb58d7296a71f6707bbd2e2e
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-03 06:01:17 -0700

    Create 03_invalid_function_overloading.cpp

---
+++ 06_Functions/03_invalid_function_overloading.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// int func(int a);      // Uncommenting both will cause a compilation error
+// double func(int a);
+
+int main() {
+    // func(5);          // Ambiguous call, 
+    return 0;
+}
