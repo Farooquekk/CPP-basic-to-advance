2e67b82109a268650c11a5c52559e117c8555d79
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-07 05:00:55 -0700

    Create summing_arrays_in_a_2_D_array.cpp

---
+++ 08_MultiDimensionalArray/summing_arrays_in_a_2_D_array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int sum = 0;
+
+   
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            sum += matrix[i][j];
+        }
+    }
+
+    cout << "Sum of all elements: " << sum << endl;  
+    return 0;
+}
