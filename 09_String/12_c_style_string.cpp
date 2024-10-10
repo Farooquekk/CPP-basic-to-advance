4bdab0813539ca7d65cf32a6273cdf4344d55c74
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-10 05:53:00 -0700

    Create 12_c_style_string.cpp

---
+++ 09_String/12_c_style_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>  // For C-string functions like strlen, strcpy, strcmp, etc.
+using namespace std;
+
+int main() {
+    // DeclareIng a C-style string
+    char str[20] = "Hello, C++!";
+    
+   
+    cout << str << endl;
+    
