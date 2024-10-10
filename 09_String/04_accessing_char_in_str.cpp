7b3400aebe43c3d9d8af00e17b4470e268935f62
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-10 05:46:12 -0700

    Create 04_accessing_char_in_str.cpp

---
+++ 09_String/04_accessing_char_in_str.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str = "Hello";
+    
+   
+    cout << "First character: " << str[0] << endl;  
+    cout << "Last character: " << str[str.length() - 1] << endl;  
+    
+    return 0;
+}
