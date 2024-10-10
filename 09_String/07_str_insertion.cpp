d8c2bd07d0339e8adaad054a7f47ce9919f8645e
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-10 05:48:16 -0700

    Create 07_str_insertion.cpp

---
+++ 09_String/07_str_insertion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str = "C++ is fun";
+    
+   
+    str.insert(4, "programming ");
+    
+    cout << str << endl;  
+    
+    return 0;
+}
