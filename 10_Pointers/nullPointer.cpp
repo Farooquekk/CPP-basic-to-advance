7c126db2621908ea846f8f974b9a72551e15fce8
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-10-21 06:38:02 -0700

    Create nullPointer.cpp

---
+++ 10_Pointers/nullPointer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int *ptr = nullptr;  // Null pointer
+
+    if (ptr == nullptr) {
+        cout << "Pointer is null (not pointing to anything)." << endl;
+    }
+
+    return 0;
+}
