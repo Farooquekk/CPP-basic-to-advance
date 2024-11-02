468954582b4b4483246402089626857f0e45718c
Farooque Sajjad <22sw040@students.muet.edu.pk>
2024-11-02 09:12:10 -0700

    Create 02_operator_overloading.cpp

---
+++ 11_OOP_in_c++/07_Polymorphism/FunctionOverloading/02_operator_overloading.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+    private:
+        int real;
+        int imag;
+
+    public:
+        Complex(int r, int i) : real(r), imag(i) {}
+
+        // Overload + operator
+        Complex operator+(const Complex& other) {
+            return Complex(real + other.real, imag + other.imag);
+        }
+
+        void display() {
+            cout << real << " + " << imag << "i" << endl;
+        }
+};
+
+int main() {
+    Complex c1(3, 4), c2(1, 2);
+    Complex c3 = c1 + c2;  // Uses overloaded + operator
+    c3.display();         
+    return 0;
+}
